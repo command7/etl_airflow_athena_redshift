f1ef0914a17e00f566555027bbd854d434d18372
command7 <vijay_saravanan@outlook.com>
2019-08-12 20:21:59 -0400

    Misc: Remove custom exceptions in a separate python file

--- Bikeshare_ETL_Exceptions.py
+++
@@ -1,8 +0,0 @@
-class ETL_Exception(Exception):
-    """Custom Exceptions for Bikeshare ETL Pipeline"""
-    pass
-
-
-class Month_Data_Missing(ETL_Exception):
-    """Raised if trips data for the current month is missing."""
-    pass
